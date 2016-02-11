06ee8cde93e3db518692c26921bde6c1d6af16cd
Herczku Misi <kmisku@gmail.com>
2016-02-11 15:26:59 +0100

    small changes

--- src/search/Test.java
+++ src/search/Test.java
@@ -8,12 +8,14 @@ public class Test
 {
 	public static void main(String[] args)
 	{
-		File mp3 = new File("C:" + File.separator + "*.mp3");
-		ID3Tag x = ID3Tag.parse(mp3);
-		System.out.println(x);
+		// File mp3 = new
+		// File("music\\01-infected_mushroom-pink_nightmares.mp3");
+		// ID3Tag x = ID3Tag.parse(mp3);
+		// System.out.println(x);
 
 		Searcher s = new Searcher();
-		System.out.println(s.matches("Magic", mp3));
+		// s.matches("pink", mp3);
+		// System.out.println(s.matches("pink", mp3));
 
 		List<File> mp3files = new ArrayList<File>();
 		mp3files.add(new File("music\\01-infected_mushroom-pink_nightmares.mp3"));
@@ -30,6 +32,13 @@ public class Test
 		mp3files.add(new File("music\\08. Spectral Seagulls.mp3"));
 		mp3files.add(new File("music\\3 Hope Was Closed.mp3"));
 
+		// when we search in everything
+		for (int i = 0; i < mp3files.size(); i++)
+		{
+			File mp3 = mp3files.get(i);
+			ID3Tag x = ID3Tag.parse(mp3);
+			s.matches("", mp3);
+		}
 	}
 
 }
